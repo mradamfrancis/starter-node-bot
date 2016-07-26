1e8659527f632a2c920be6426714ee706ee4fdc4
mradamfrancis <ajfrancis@gmail.com>
2016-07-26 11:55:31 +0100

    test

--- index.js
+++ index.js
@@ -44,6 +44,19 @@ controller.hears(['send flowers'], ['direct_mention'], function (bot, message) {
   });
 })
 
+controller.hears(['question me'], 'message_received', function(bot,message) {
+  // start a conversation to handle this response.
+  bot.startConversation(message,function(err,convo) {
+    convo.ask('How are you?',function(response,convo) {
+      convo.say('Cool, you said: ' + response.text);
+      convo.next();
+
+    });
+
+  })
+
+})
+
 controller.hears(['flowertime'], 'message_received', function(bot,message) {
     askFlavor = function(response, convo) {
       convo.ask('What flower do you want? Zara, Charlie or Willow', function(response, convo) {
