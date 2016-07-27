1f32afaa0b7b49f22c1472d49fe336bc8977b847
mradamfrancis <ajfrancis@gmail.com>
2016-07-27 17:26:59 +0100

    sd

--- index.js
+++ index.js
@@ -304,7 +304,7 @@ convo.ask("Type 'YES' to approve your order.", function(response, convo) {
 }
 
 q19 = function(response, convo) {
-convo.ask(“Use your card ending XX339?:money_mouth_face:", function(response, convo) {
+convo.ask(“Use your card ending XX339. :money_mouth_face:", function(response, convo) {
     
         convo.say(“Brill:nerd_face:”)
         q20(response, convo);
